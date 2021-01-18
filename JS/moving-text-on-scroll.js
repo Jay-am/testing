03f6f1a93994a455745dadd66e0256b8871079e7
Julia Mackamul <julia.mackamul@gmail.com>
2021-01-18 20:41:35 +0100

    structure navigation

--- JS/moving-text-on-scroll.js
+++ JS/moving-text-on-scroll.js
@@ -1,4 +1,6 @@
 $(document).on('scroll', function() {
   $('intro-2').css("left", Math.max(500 - 0.35*window.scrollY) + "px");
-  $('collaborations__type--1').css("left", Math.max(500 - 0.35*window.scrollY, 100) + "px");
+  // $('intro-2').css("left", Math.max(1400 - 0.35*window.scrollY) + "px");
+  // $('collaborations__type--1').css("left", Math.max(500 - 0.35*window.scrollY, 100) + "px");
+  // $('collaborations__type--1').css("left", Math.max(500 - 0.35*window.scrollY, 0) + "px");
 })
