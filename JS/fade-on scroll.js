0ecda0db792e26f7d94de289db7bab4f2678925b
Julia Mackamul <julia.mackamul@gmail.com>
2021-01-21 13:50:48 +0100

    added js links to all html files

--- JS/fade-on scroll.js
+++
@@ -1,8 +0,0 @@
-var divs = $('.climate-change, .vision');
-$(window).scroll(function(){
-   if($(window).scrollTop()<1){
-         divs.stop(true,true).fadeIn("slow");
-   } else {
-         divs.stop(true,true).fadeOut("slow");
-   }
-});
--- JS/test.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-      var wScroll = $(this).scrollTop();
-
-      $('wholesale__headline').css({
-          'transform' : 'translateX('wScroll+'px)'
-      });
-  });
-});
