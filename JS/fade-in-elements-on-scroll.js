85280127d43f8121a384f872102ee3640a4b2c37
Julia Mackamul <julia.mackamul@gmail.com>
2021-02-04 18:16:06 +0100

    added fade-in effect to index

--- JS/fade-in-elements-on-scroll.js
+++ JS/fade-in-elements-on-scroll.js
@@ -41,15 +41,15 @@
 
 $(document).ready(function() {
   $(window).scroll( function(){
-      $('.hideme').each( function(i){
-          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-          var bottom_of_window = $(window).scrollTop() + $(window).height();
-          if( bottom_of_window > bottom_of_object ){
-              $(this).addClass('showme');
-          }
-          if( bottom_of_window < bottom_of_object ){
-              $(this).removeClass('showme');
-          }
-      });
+    $('.fade-in').each( function(i){
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      if( bottom_of_window > bottom_of_object ){
+        $(this).addClass('fade-out');
+      }
+      if( bottom_of_window < bottom_of_object ){
+        $(this).removeClass('fade-out');
+      }
+    });
   });
 });
