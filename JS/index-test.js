4e905ad531acc749bc70729451d7d380f9fa1819
Julia Mackamul <julia.mackamul@gmail.com>
2021-02-04 22:48:13 +0100

    clipping headlines index

--- JS/index-test.js
+++ JS/index-test.js
@@ -1,60 +1,60 @@
-// (function($) {
+(function($) {
 
-//   $.fn.visible = function(partial) {
+  $.fn.visible = function(partial) {
     
-//       var $t            = $(this),
-//           $w            = $(window),
-//           viewTop       = $w.scrollTop(),
-//           viewBottom    = viewTop + $w.height(),
-//           _top          = $t.offset().top,
-//           _bottom       = _top + $t.height(),
-//           compareTop    = partial === true ? _bottom : _top,
-//           compareBottom = partial === true ? _top : _bottom;
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
     
-//     return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
 
-//   };
+  };
     
-// })(jQuery);
+})(jQuery);
 
-// var win = $(window);
+var win = $(window);
 
-// var allMods = $(".fade-in");
+var allMods = $(".fade-in");
 
-// allMods.each(function(i, el) {
-//   var el = $(el);
-//   if (el.visible(true)) {
-//     el.addClass("already-visible"); 
-//   } 
-// });
-
-// win.scroll(function(event) {
-  
-//   allMods.each(function(i, el) {
-//     var el = $(el);
-//     if (el.visible(true)) {
-//       el.addClass("come-in"); 
-//     } 
-//   });
-  
-// });
-
-
-window.addEventListener('scroll', function(e) {
-
-  const target = document.querySelectorAll('.fade-in');
-  var index = 0, length = target.length;
-  
-  for (index; index < length; index++) {
-    var pos = window.pageYOffset * target[index].dataset.rate;
-
-    if(target[index].dataset.direction === 'vertical') {
-        target[index].style.transform = 'translate3d(0px,'+pos+'px, 0px)';
-    } else {
-        var posX = window.pageYOffset * target[index].dataset.ratex;
-        var posY = window.pageYOffset * target[index].dataset.ratey;
-          
-        target[index].style.transform = 'translate3d('+posX+'px, '+posY+'px, 0px)';
-    }
-  }
+allMods.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible"); 
+  } 
 });
+
+win.scroll(function(event) {
+  
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in"); 
+    } 
+  });
+  
+});
+
+
+// window.addEventListener('scroll', function(e) {
+
+//   const target = document.querySelectorAll('.fade-in');
+//   var index = 0, length = target.length;
+  
+//   for (index; index < length; index++) {
+//     var pos = window.pageYOffset * target[index].dataset.rate;
+
+//     if(target[index].dataset.direction === 'vertical') {
+//         target[index].style.transform = 'translate3d(0px,'+pos+'px, 0px)';
+//     } else {
+//         var posX = window.pageYOffset * target[index].dataset.ratex;
+//         var posY = window.pageYOffset * target[index].dataset.ratey;
+          
+//         target[index].style.transform = 'translate3d('+posX+'px, '+posY+'px, 0px)';
+//     }
+//   }
+// });
