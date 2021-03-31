9d3f74316c289deb4b6ee8d05b18a55779de7709
Julia Mackamul <julia.mackamul@gmail.com>
2021-03-31 10:43:16 +0200

    new structure coverage

--- JS/coverage-counter.js
+++ JS/coverage-counter.js
@@ -1,36 +1,46 @@
-function Counter(selector){
-  this.el      = $(selector);
-  this.numbers = this.el.find('> b').toArray().reverse();
-}
+// function Counter(selector){
+//   this.el      = $(selector);
+//   this.numbers = this.el.find('> b').toArray().reverse();
+// }
 
-Counter.prototype.count = function(newVal){
-  var reversedArr, className;
-  // update instance's value
-  this.value = newVal || this.el.attr('data-value')|0;
+// Counter.prototype.count = function(newVal){
+//   var reversedArr, className;
+//   // update instance's value
+//   this.value = newVal || this.el.attr('data-value')|0;
    
-  if( !this.value ) return;
-  // convert value into an array of numbers
-  reversedArr = (this.value+'').split('').reverse();
-  // loop on each number element and change it
-  this.numbers.forEach(function(item, i){ 
-      setTimeout(function(){
-          className = 'd' + reversedArr[i] || 0;
-          //if( item.className != className)
-              item.className = className;
-      }, i * 250);
-  })
-}
+//   if( !this.value ) return;
+//   // convert value into an array of numbers
+//   reversedArr = (this.value+'').split('').reverse();
+//   // loop on each number element and change it
+//   this.numbers.forEach(function(item, i){ 
+//       setTimeout(function(){
+//           className = 'd' + reversedArr[i] || 0;
+//           //if( item.className != className)
+//               item.className = className;
+//       }, i * 250);
+//   })
+// }
 
-/////////////// create new counter for this demo ///////////////////////
+// var counter = new Counter('.numCounter');
+// randomCount();
 
-var counter = new Counter('.numCounter');
-randomCount();
+// function randomCount(){
+//   counter.count( randomNum(50,9999999) );
+//   setTimeout(randomCount,4000);
+// }
 
-function randomCount(){
-  counter.count( randomNum(50,9999999) );
-  setTimeout(randomCount,4000);
-}
+// function randomNum(min,max){
+//     return Math.floor(Math.random()*(max-min+1)+min);
+// }
 
-function randomNum(min,max){
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
+$('.count').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 2000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  });
+}); 
